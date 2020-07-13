899bf34fa65d58eca3e3dba218c2a1c5986030cb
tvkent <tvkent@ucdavis.edu>
2020-07-13 18:16:24 -0400

    added rate calc

--- interpolate.R
+++ interpolate.R
@@ -3,6 +3,8 @@
 library(magrittr,quietly = T)
 library(dplyr,quietly = T,warn.conflicts = F)
 library(stats,quietly = T)
+library(data.table,quietly = T)
+options(scipen = 999)
 
 args <- commandArgs(TRUE)
 
@@ -10,9 +12,18 @@ args <- commandArgs(TRUE)
 pos<-read.table(args[1])
 map<-read.table(args[2])
 
+#remove positions smaller and greater than min/max map pos
+minpos <- min(map$V2)
+maxpos <- max(map$V2)
+spos <- data.frame(position=pos[pos$V1 > minpos & pos$V1 < maxpos,])
+subpos <- rbind(spos,maxpos)
 #calc monotonic interpolation
 interp<-splinefun(map$V2,map$V3,method='monoH.FC')
-chpos<- pos %>% mutate(gen=interp(x=pos$V1))
+chpos<- subpos %>% mutate(gen=interp(x=subpos$position))
+
+setDT(chpos)
+chpos[,rate:=(shift(gen,1,type="lead")-gen)/(shift(position,1,type="lead")-position)]
+setDF(chpos)
 
 #output to stdout
-write.table(chpos,'',quote = F, row.names = F,col.names = F)
+write.table(na.omit(chpos),'',quote = F, row.names = F,col.names = F)
